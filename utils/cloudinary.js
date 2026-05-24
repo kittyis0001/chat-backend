430f11030c1962232310bc7d080444cbbd0cf375
kittyis0001 <kittyis0001@gmail.com>
2026-05-25 02:23:57 +0600

    Configure Cloudinary with API credentials

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,1 +1,9 @@
+const cloudinary = require("cloudinary").v2
 
+cloudinary.config({
+  cloud_name: "dlnomi3ny",
+  api_key: "576225117181211",
+  api_secret: "CB0duPoZmja7TyXz-4G_h1GlGkY"
+})
+
+module.exports = cloudinary
